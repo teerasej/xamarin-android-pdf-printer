8003a1808ce48910f7ee6c2c8ff7cdc5f2729ced
Teerasej <teerasej@gmail.com>
2021-04-30 16:25:37 +0700

    สร้าง Download Service สำหรับดาวน์โหลดไฟล์

---
+++ DownloadService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace PDFPrinter
+{
+    public class DownloadService
+    {
+        public string DestinationPath;
+        public WebClient webClient;
+
+        public DownloadService(string destinationPath)
+        {
+            DestinationPath = destinationPath;
+            webClient = new WebClient();
+        }
+
+        public async Task<string> Download(string targetFileUrl)
+        {
+            var targetFileName = Path.GetFileName(targetFileUrl);
+            var localFilePath = Path.Combine(DestinationPath, targetFileName);
+
+            await webClient.DownloadFileTaskAsync(targetFileUrl, localFilePath);
+
+            return localFilePath;
+        }
+    }
+}
