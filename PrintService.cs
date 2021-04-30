15bd05ea7b87cb5f2cd6fef5848883a1543e55c9
Teerasej <teerasej@gmail.com>
2021-04-30 22:12:30 +0700

    เพิ่ม printer service

---
+++ PrintService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using LinkOS.Plugin;
+using LinkOS.Plugin.Abstractions;
+using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
+
+namespace PDFPrinter
+{
+    public class PrintService
+    {
+        IConnection connection;
+        List<IDevice> devices;
+
+        public PrintService()
+        {
+            devices = new List<IDevice>();
+        }
+
+        public async void Connect()
+        {
+            var ble = CrossBluetoothLE.Current;
+            var bluetoothStatus = ble.State;
+
+            if (bluetoothStatus == BluetoothState.On)
+            {
+                var adapter = CrossBluetoothLE.Current.Adapter;
+
+                var devices = adapter.GetSystemConnectedOrPairedDevices();
+
+                return;
+            }
+        }
+    }
+}
