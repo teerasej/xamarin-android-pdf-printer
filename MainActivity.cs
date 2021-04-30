b0717d10e0c325a30f230585ccc54644a83a8789
Teerasej <teerasej@gmail.com>
2021-04-30 16:25:19 +0700

    เรียกใช้ View ใน MainActivity

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 
 namespace PDFPrinter
@@ -8,12 +9,20 @@ namespace PDFPrinter
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        Button buttonDownloadPDF;
+        Button buttonViewPDF;
+        Button buttonPrintPDF;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            buttonDownloadPDF = FindViewById<Button>(Resource.Id.buttonDownloadPDF);
+            buttonViewPDF = FindViewById<Button>(Resource.Id.buttonViewPDF);
+            buttonPrintPDF = FindViewById<Button>(Resource.Id.buttonPrintPDF);
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
