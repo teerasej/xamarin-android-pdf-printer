216cbd69b671a0462554317ae7f165f2fad67a5a
Teerasej <teerasej@gmail.com>
2021-04-30 16:38:24 +0700

    ดาวน์โหลดไฟล์มาไว้ในเครื่อง และเปิดปุ่มที่เหลือ

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,3 +1,5 @@
+﻿using System;
+using System.IO;
 using Android.App;
 using Android.OS;
 using Android.Runtime;
@@ -13,6 +15,8 @@ namespace PDFPrinter
         Button buttonViewPDF;
         Button buttonPrintPDF;
 
+        Uri localPDFFile;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -25,6 +29,20 @@ namespace PDFPrinter
 
             buttonViewPDF.Enabled = false;
             buttonViewPDF.Enabled = false;
+
+            buttonDownloadPDF.Click += async (sender, e) =>
+            {
+                var targetFileUrl = "https://www.nextflow.in.th/demo/xamarin-android/files/flutter.pdf";
+
+                var localAppDataFolderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData);
+                var downloader = new DownloadService(localAppDataFolderPath);
+
+                localPDFFile = await downloader.Download(targetFileUrl);
+
+                buttonViewPDF.Enabled = true;
+                buttonViewPDF.Enabled = true;
+            };
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
